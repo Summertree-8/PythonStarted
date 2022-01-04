3f77b5207c484b27ab8032e87b96840c651e347c
Summertree-8 <goldensapphire2eternity-cherry@yahoo.co.jp>
2022-01-04 16:22:41 +0900

    [add] python practice basic 11~

--- practice_basics.py
+++ practice_basics.py
@@ -68,10 +68,102 @@ print(type(str(x)))
 print(str(x))
 
 #11
+a,b = 'sba', 'gasba'
+print(a in b)
+a,b = 'xdh', 'xjake'
+print(a in b)
 
+#12
+numbers=[0,3,8,-4,9,1]
+print(numbers[1])
+print(numbers[-1])
+numbers.append(2)
+print(numbers)
 
+#13
+numbers.insert(0,5)
+print(numbers)
+numbers.insert(-1,-3)
+print(numbers)
 
+#14
+#delete he number itself
+print(numbers)
+numbers.remove(5)
+print(numbers)
+numbers.remove(3)
+print(numbers)
+#delete index
+numbers.pop(2)
+print(numbers)
 
+#15
+numbers = [0,8,-4,9,-3,2]
+def isEven(number):
+    if number % 2 == 0:
+        print(f'This number, {number} is even')
+        return True
+    else:
+        print(f'This number, {number} is odd')
+        return False
+
+print(isEven(2))
+
+print(list(filter(isEven, numbers)))
+
+#16
+numbers = [0,8,-4,9,-3,2]
+print(numbers.index(-4))
+
+#17
+numbers.sort()
+print(numbers)
+numbers.sort(reverse=True)
+print(numbers)
+
+#18
+dictionary = {
+    'A': 'apple',
+    'B': 'cherry',
+    'C': 'kwi',
+    'D': 'water',
+    'E': 'melon'
+}
+print(dictionary)
+dictionary.pop('A')
+print(dictionary)
+dictionary.pop('B')
+print(dictionary)
+dictionary.clear()
+print(dictionary)
+
+#19
+dictionary = {
+    'A': 'apple',
+    'B': 'cherry',
+    'C': 'kwi',
+    'D': 'water',
+    'E': 'melon'
+}
+print(dictionary.keys())
+print(dictionary.values())
+print('apple' in dictionary.values())
+for key, value in dictionary.items():
+    print(f'key is {key}, value is {value}')
+
+#20
+dictionary = {
+    'A': 'apple',
+    'B': 'cherry',
+    'C': 'kwi',
+    'D': 'water',
+    'E': 'melon'
+}
+
+print(dictionary['C'])
+# print(dictionary['F'])
+#
+print(dictionary.get('F'))
 
 
 
