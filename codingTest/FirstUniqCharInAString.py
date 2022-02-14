a6b20412374f5ae4b0702e08fa125df6c37c5fea
Summertree-8 <goldensapphire2eternity-cherry@yahoo.co.jp>
2022-02-14 22:30:56 +0900

    [add] coding test practice

---
+++ codingTest/FirstUniqCharInAString.py
@@ -0,0 +1,10 @@
+import collections
+
+
+class Solution:
+    def firstUniqChar(self, s):
+        count = collections.Counter(s)
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+        return -1
