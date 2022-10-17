e5f0d6e7dd25e9b184825d0125b361af69d2f6f7
Summertree-8 <goldensapphire2eternity-cherry@yahoo.co.jp>
2022-10-17 22:35:50 +0900

    [add] tf-idf: tf, idf, tf-idfの計算

--- TF-IDF/tf_idf.py
+++ TF-IDF/tf_idf.py
@@ -15,7 +15,7 @@ def read_file(path):
 
   return word_list
 
-#tf（「単語 i の文書 j における出現回数）を計算し辞書で返す
+#渡された辞文書の各単語のtf（「単語 i の文書 j における出現回数）を計算し辞書で返す
 def tf(doc):
   word_count = {}
   for i in doc:
@@ -23,6 +23,7 @@ def tf(doc):
   # print(word_count)
   return word_count
 
+#渡された単語のdfの値を返す
 def calc_df(word, docs):
   df = 0
   for doc in docs:
@@ -30,18 +31,19 @@ def calc_df(word, docs):
       df += 1
   return df
 
-#IDFを計算し辞書で返す
-def idf(word, docs):
-  #文書数
-  N = 10
-  #単語iが出現する文書の数
-  df = calc_df(word, docs)
-  #idfを計算し返す
-  return math.log2(N/df)
+#渡された文書の各単語のIDFを計算し辞書で返す
+def idf(N, doc, docs):
+  idf = {}
+  for word in doc:
+    idf[word] = math.log2(N/calc_df(word, docs))
+  return idf
 
-#TF-IDFの計算
-def tf_idf():
-  return tf*idf
+#渡された文書のTF-IDFの計算
+def tf_idf(doc, tf_score, idf_score):
+  tf_idf = {}
+  for word in doc:
+    tf_idf[word] = tf_score[word]*idf_score[word]
+  return tf_idf
 
 def write_file(path, text):
   try:
@@ -50,22 +52,36 @@ def write_file(path, text):
     file.close()
   except:
     text = "file write error"
+    print('error')
 
 def main():
-  #全ての文書を保持
+  #文書の数
+  N = 3
+  #全ての文書を読み込む
   docs = []
-  for i in range(10):
-    docs.append(read_file('input/'+str(i)+'.txt'))
-  # print(docs)
-  list_2 = read_file('0_1.txt')
-  # print(list_a)
-  # tf_score = tf(list_1)
-  # for i in list_a:
-  #   tf_score[i] = tf(i, list_a)
-  # print(tf_score)
-  idf_score = {}
-  # for i in list_a:
-  #   idf_score[i] = tf(i, docs)
+  for i in range(N):
+    # docs.append(read_file('input/'+str(i)+'.txt'))
+    docs.append(read_file(str(i) + '.txt'))
+  print(docs)
+
+  tf_score = []
+  for doc in docs:
+    tf_score.append(tf(doc))
+  print(tf_score)
+
+  idf_score = []
+  for doc in docs:
+    idf_score.append(idf(N, doc, docs))
+  print(idf_score)
+
+  tf_idf = []
+  for i in range(N):
+    tf_idf.append(tf_idf(doc[i], tf_score[i], idf_score[i]))
+  print(tf_idf)
+
+  # for i in range(N):
+  #   text =
+  #   write_file('result/'+str(i)+'.txt', text)
 
 if __name__ == "__main__":
   main()
