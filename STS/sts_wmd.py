39c46921e9726bc0d2c71528da570419488f26bd
Summertree-8 <goldensapphire2eternity-cherry@yahoo.co.jp>
2022-11-16 01:05:11 +0900

    [add] sts week3: Word Mover's Distance version

---
+++ STS/sts_wmd.py
@@ -0,0 +1,123 @@
+#整形＋ストップワード除去＋語幹化
+import codecs
+import math
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer as PS
+
+from gensim.models.keyedvectors import KeyedVectors
+
+
+#ファイルの読み込みlistで返す
+def read_file(path):
+  try:
+    file = codecs.open(path, 'r', encoding='utf-8')
+    text = file.readlines()
+    file.close()
+    # print('read')
+  except:
+    text = "file open error"
+
+  # word_list = text.split()
+  return text
+
+def tf_idf(docs):
+  # モデルの生成
+  vectorizer = TfidfVectorizer(smooth_idf=False)
+
+  # TF-IDFの計算
+  values = vectorizer.fit_transform(docs).toarray()
+  # print('feature_names:', vectorizer.get_feature_names_out())
+
+  # 特徴量ラベルの取得
+  words = vectorizer.get_feature_names_out()
+
+  return values, words
+
+#cos類似度を求める
+# def cos_sim(v1, v2):
+#   return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+def write_file(path, text):
+  try:
+    file = codecs.open(path, "a", "utf-8")
+    file.write(text)
+    file.close()
+  except:
+    text = "file write error"
+    print('error')
+
+def main():
+  #read files
+  docs = []
+  docs=(read_file('testData/STS.input.images.txt'))
+  # print(docs)
+  doc1 = []
+  doc2 = []
+  doc_all = []
+  # doc1 = np.array([])
+  # doc2 = np.array([])
+  stop_words = stopwords.words('english')
+  ps = PS()
+  for i in range(len(docs)):
+    # doc1.append(docs[i].split('\t')[0])
+    # doc2.append(docs[i].split('\t')[1])
+    doc_all.append(docs[i].split('\t')[0])
+    doc_all.append(docs[i].split('\t')[1])
+    # doc1 = np.append(doc1, docs[i].split('\t')[0])
+    # doc2 = np.append(doc2, docs[i].split('\t')[1])
+  print('doc_all: \n', doc_all)
+  doc_all_new = []
+
+  for i in range(len(doc_all)):
+    _tmp = doc_all[i].replace('.', '')
+    tmp = _tmp.split()
+    tmp2 = ''
+    # print(tmp)
+    for j in range(len(tmp)):
+      if tmp[j] not in stop_words:
+        tmp2 += ps.stem(tmp[j]) + ' '
+    doc_all_new.append(tmp2)
+  print('doc_all_new: \n', doc_all_new)
+
+  for i in range(int(len(doc_all_new)/2)):
+    doc1.append(doc_all_new[i*2])
+    doc2.append(doc_all_new[i*2+1])
+  print(doc1)
+  print(doc2)
+
+  #calc tf-idf
+  tf_idf_list = []
+  tf_idf_list, tf_idf_words = tf_idf(doc_all_new)
+  tf_idf1 = []
+  tf_idf2 = []
+  # print('tf_idf_list: \n', tf_idf_list)
+  # print('tf_idf_words: \n', tf_idf_words)
+  for i in range(len(docs)):
+    tf_idf1.append(tf_idf_list[i * 2])
+    tf_idf2.append(tf_idf_list[i * 2 + 1])
+  # print('tf_idf1: \n', tf_idf1)
+  # print('tf_idf2: \n', tf_idf2)
+
+  model = KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin', binary=True)
+
+  #calc similarity by wmd
+  for i in range(len(docs)):
+    print(model.wmdistance(doc1[i], doc2[i]))
+    write_file('result.txt', str(model.wmdistance(doc1[i],doc2[i])) + '\n')
+
+  # print('cos_sim_res: \n', cos_sim_res)
+  # print('res: \n', res)
+
+  print(len(docs))
+  print(len(doc_all))
+  print(len(tf_idf1))
+  print(len(tf_idf2))
+
+
+
+if __name__ == "__main__":
+  main()
